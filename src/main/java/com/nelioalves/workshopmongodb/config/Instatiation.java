a19ab601654499dbf817b6d9e1fc2359e2e7b78f
amercador <amercador@hotmail.com>
2021-03-11 14:43:27 -0300

    Referenciando os posts do usuário

--- src/main/java/com/nelioalves/workshopmongodb/config/Instatiation.java
+++ src/main/java/com/nelioalves/workshopmongodb/config/Instatiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nelioalves.workshopmongodb.domain.Post;
 import com.nelioalves.workshopmongodb.domain.User;
+import com.nelioalves.workshopmongodb.dto.AuthorDTO;
 import com.nelioalves.workshopmongodb.repository.PostRepository;
 import com.nelioalves.workshopmongodb.repository.UserRepository;
 
@@ -35,11 +36,15 @@ public class Instatiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("28/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("28/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 		
 	}
 
--- src/main/java/com/nelioalves/workshopmongodb/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongodb/domain/Post.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.nelioalves.workshopmongodb.dto.AuthorDTO;
+
 @Document
 public class Post {
 	
@@ -13,12 +15,12 @@ public class Post {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -50,10 +52,10 @@ public class Post {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
