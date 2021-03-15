ce9979bf7ea169f46eea5fcf5ba059428a35633d
amercador <amercador@hotmail.com>
2021-03-15 10:59:35 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nelioalves/workshopmongodb/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package com.nelioalves.workshopmongodb.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.workshopmongodb.dto.AuthorDTO;
+import com.nelioalves.workshopmongodb.dto.CommentDTO;
 
 @Document
 public class Post {
@@ -16,6 +19,7 @@ public class Post {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -57,6 +61,14 @@ public class Post {
 	}
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
